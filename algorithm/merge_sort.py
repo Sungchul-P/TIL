2fdaf613f3c8fe078eb5bc4aa4877afa2d26e10d
Sungchul-P <sungchul.dev@gmail.com>
2019-05-07 20:58:38 +0900

    [add] 병합 정렬 주석 추가

--- algorithm/merge_sort.py
+++ algorithm/merge_sort.py
@@ -1,13 +1,21 @@
+'''
+[병합 정렬 알고리즘]
+- 이미 정렬되어 있는 데이터들을 하나로 합쳐서 정렬하는 방법이다.
+- 파일에 정렬되어 있는 데이터들을 하나로 합쳐서 정렬하는 경우에도 종종 사용된다.
+'''
+
 #!/usr/bin/python
 from math import log10
 from random import randint
 import sys
 
+# 최소한의 데이터를 갖는 런이 될 때까지 재귀 호출이 된다.
 def merge_sort(mylist):
     if len(mylist) <= 1: return mylist
-    half = len(mylist) // 2
-    left_list = merge_sort(mylist[:half])
-    right_list = merge_sort(mylist[half:])
+    half = len(mylist) // 2 # 중간 값
+
+    left_list = merge_sort(mylist[:half]) # 각 런의 가장 왼쪽 데이터
+    right_list = merge_sort(mylist[half:]) # 오른쪽 데이터
     merged_list = []
 
     while len(left_list) > 0 and len(right_list) > 0:
