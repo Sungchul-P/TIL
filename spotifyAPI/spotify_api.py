a254d148ab65f8a28851bcd2c7a5b3fa6e0c55a1
sungchul-p <sungchul.dev@gmail.com>
2019-11-28 00:34:24 +0900

    [ADD] 에러 핸들링 및 아티스트 앨범 API 사용

--- spotifyAPI/spotify_api.py
+++ spotifyAPI/spotify_api.py
@@ -3,9 +3,10 @@ import requests
 import base64
 import json
 import logging
+import time
 
-clientID = ""
-clientSecret = ""
+clientID = "3021168ce0dc4c92a62b744e80be2030"
+clientSecret = "377a424cc0dd479db7bd56ac1f44419c"
 
 def main():
     headers = getHeaders(clientID, clientSecret)
@@ -17,10 +18,67 @@ def main():
         "limit": "5"
     }
 
-    r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+    try:
+        r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+    
+    except:
+        logging.error(r.text)
+        sys.exit(1)
 
-    print(r.status_code)
-    print(r.text)
+    # StatusCode Error Handling
+    if r.status_code != 200:
+        logging.error(r.text)
+
+        # 429 Code : Too Many Request
+        if r.status_code == 429:
+            # 대기 시간을 Retry-After로 반환한다.
+            retryAfter = json.loads(r.headers)['Retry-After']
+            time.sleep(int(retryAfter))
+
+            requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+        # 401 Code : AccessToken expired
+        elif r.status_code == 401:
+            headers = getHeaders(clientID, clientSecret)
+            requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+        else:
+            sys.exit(1)
+
+    # Spotify Get an Artist's Albums API
+    artistID = "6OwKE9Ez6ALxpTaKcT5ayv"
+    r = requests.get("https://api.spotify.com/v1/artists/{id}/albums".format(id=artistID), headers=headers)
+
+    raw = json.loads(r.text)
+
+    total = raw['total']
+    offset = raw['offset']
+    limit = raw['limit']
+    next = raw['next'] # 다음 요청 해야 할 Endpoint
+
+    print("total : {}\noffset : {}\nlimit : {}\nnext : {}".format(total, offset, limit, next))
+
+    albums = []
+    print(len(raw['items']))
+    albums.extend(raw['items'])
+
+    # 앨범 n 개 정보만 조회
+    n = 100
+    count = 0
+    while count < n and next:
+        r = requests.get(raw['next'], headers=headers)
+        raw = json.loads(r.text)
+        next = raw['next']
+        print(next)
+
+        albums.extend(raw['items'])
+        count = len(albums)
+
+    print(len(albums))
+
+    # print(r.status_code)
+    # print(r.text)
+    # print(r.headers)
     
 
 def getHeaders(clientID, clientSecret):
